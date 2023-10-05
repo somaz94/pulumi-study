1a7f0b374007d8490d925d7a6008afaf4ad15ba8
somaz <genius5711@gmail.com>
2023-10-05 14:34:51 +0900

    refactor: bigdata-flow-functions

--- gcp/python/bigdata-flow-functions/bigquery-to-google-sheet/main.py
+++ gcp/python/bigdata-flow-functions/bigquery-to-google-sheet/main.py
@@ -4,11 +4,11 @@ import datetime
 from google.cloud import bigquery
 from oauth2client.service_account import ServiceAccountCredentials
 import logging
-import pytz  # Import the pytz library for timezone handling
+import pytz
 
-MAX_ROWS_PER_BATCH = 1000  # Define the maximum number of rows to process at once.
+MAX_ROWS_PER_BATCH = 1000
 
-def bigquery_to_sheets(request, desired_date="2023-06"): # Modify Date(ex.2023-06, 2023-07 ....)
+def bigquery_to_sheets(request, desired_date="2023-06-09"): # Modify Date(ex.2023-06-01, 2023-07-01 ....)
     try:
         logging.info("Starting bigquery_to_sheets function")
 
@@ -25,46 +25,44 @@ def bigquery_to_sheets(request, desired_date="2023-06"): # Modify Date(ex.2023-0
 
         # Connect to Google Sheets
         scopes = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-        creds = ServiceAccountCredentials.from_json_keyfile_name('bigquery.json', scopes)    # bigquery and cloud function and cloud scheduler and dataflow admin service account json file   
+        creds = ServiceAccountCredentials.from_json_keyfile_name('bigquery.json', scopes)
         gc = gspread.authorize(creds)
         logging.info("Connected to Google Sheets")
 
         # Open Google Sheets document
         sh = gc.open_by_key('1xxxxxxxxxxxxxxxXvB_M...')     # Google Sheet ID
 
+        # Date filter preparation
+        if "-" in desired_date and len(desired_date.split("-")) == 3:  # YYYY-MM-DD format check
+            # Fetch data for that specific date
+            date_filter = f"DATE(PARSE_TIMESTAMP('%a %b %d %H:%M:%S UTC %Y', time)) = '{desired_date}'"
+        else:
+            logging.warning(f"Invalid desired_date format: {desired_date}. Should be in 'YYYY-MM-DD' format.")
+            return f"Invalid desired_date format: {desired_date}. Should be in 'YYYY-MM-DD' format.", 400
+
+        # Filter for the 'reason' column
+        reason_filter = "(reason = '/login' OR reason = '/login/unity')"
+
+        # Combine date and reason filters
+        combined_filter = f"{date_filter} AND {reason_filter}"
+
         for table in tables:
             table_id = table.table_id
             database_name = table_id.split('-')[0]
-
-            # Date filter preparation
-            if "-" in desired_date and len(desired_date.split("-")) == 2:
-                # Fetch the entire month's data
-                date_filter = f"FORMAT_DATE('%Y-%m', DATE(PARSE_TIMESTAMP('%a %b %d %H:%M:%S UTC %Y', time))) = '{desired_date}'"
-            else:
-                # Fetch data for that specific date
-                date_filter = f"DATE(PARSE_TIMESTAMP('%a %b %d %H:%M:%S UTC %Y', time)) = '{desired_date}'"
-
-            # Filter for the 'reason' column
-            reason_filter = "(reason = '/login' OR reason = '/login/unity')"
-
-            # Combine date and reason filters
-            combined_filter = f"{date_filter} AND {reason_filter}"
+            worksheet_title = f"{desired_date} {database_name}"
 
             # Calculate the total number of rows in the table for the desired date and reason
             total_rows_query = f"SELECT COUNT(*) FROM `{dataset_id}.{table_id}` WHERE {combined_filter}"
             total_rows = client.query(total_rows_query).result().to_dataframe().iloc[0, 0]
 
-            # Calculate the number of batches
-            num_batches = -(-total_rows // MAX_ROWS_PER_BATCH)  # Ceiling division
+            num_batches = -(-total_rows // MAX_ROWS_PER_BATCH)
 
-            # Check if worksheet exists, if not create one
             try:
-                worksheet = sh.worksheet(database_name)
-                worksheet.clear()  # Clear data if sheet already exists
+                worksheet = sh.worksheet(worksheet_title)
+                worksheet.clear()
             except gspread.exceptions.WorksheetNotFound:
-                worksheet = sh.add_worksheet(title=database_name, rows=str(total_rows+1), cols="20")
+                worksheet = sh.add_worksheet(title=worksheet_title, rows=str(total_rows+1), cols="20")
 
-            # Add schema (column names) to the first row
             schema = [field.name for field in client.get_table(f"{dataset_id}.{table_id}").schema]
             worksheet.append_row(schema)
 
@@ -78,7 +76,6 @@ def bigquery_to_sheets(request, desired_date="2023-06"): # Modify Date(ex.2023-0
                     row_data = []
                     for item in row.values():
                         if isinstance(item, datetime.datetime):
-                            # Convert timestamp to 'Asia/Seoul' timezone
                             seoul_timezone = pytz.timezone('Asia/Seoul')
                             localized_timestamp = item.replace(tzinfo=pytz.utc).astimezone(seoul_timezone)
                             formatted_timestamp = localized_timestamp.strftime('%Y-%m-%d %H:%M:%S')
@@ -87,7 +84,6 @@ def bigquery_to_sheets(request, desired_date="2023-06"): # Modify Date(ex.2023-0
                             row_data.append(item)
                     batch_data.append(row_data)
 
-                # Use batch update for better performance
                 worksheet.append_rows(batch_data)
 
         logging.info("Data synced to Google Sheets!")
@@ -96,3 +92,4 @@ def bigquery_to_sheets(request, desired_date="2023-06"): # Modify Date(ex.2023-0
     except Exception as e:
         logging.error(f"Error occurred: {e}")
         raise e
+
